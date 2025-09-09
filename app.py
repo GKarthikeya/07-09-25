000513ed28ad23d1e669a99c67fe0f13829d0a3d
GKarthikeya <128343681+GKarthikeya@users.noreply.github.com>
2025-09-10 02:41:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def streak():
 
 
 # ---------------------------
-# Run app (local only)
+# Local run only
 # ---------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
